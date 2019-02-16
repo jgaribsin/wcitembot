94638efc4f8159aeede1ee562e8e7b5ce99152fd
Major-Lue <Major-Lue@users.noreply.github.com>
2019-02-16 02:45:04 -0700

    added activity message

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const { RichEmbed } = require('discord.js');
 
 client.on('ready', () => {
     console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
+    client.user.setActivity("with silver's heart", {type: 'PLAYING'});
     // uptimeUpdate();
 });
 
