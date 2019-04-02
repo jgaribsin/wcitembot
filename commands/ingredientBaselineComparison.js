95b9107cfa02b5dd084880d2c4698f2e676e995a
Major-Lue <Major-Lue@users.noreply.github.com>
2019-04-01 23:35:40 -0600

    Fixed a bug where baseline command didn't pass the correct job.

--- commands/ingredientBaselineComparison.js
+++ commands/ingredientBaselineComparison.js
@@ -53,7 +53,7 @@ else {
   let identifications = Object.keys(foundIngr.identifications);
   let priorityJob = "";
   let jobsKeys = Object.keys(foundIngr.skills);
-  if (jobsKeys.length = 1) priorityJob = foundIngr.skills[0];
+  if (jobsKeys.length == 1) priorityJob = foundIngr.skills[0];
   else priorityJob = fnc.sortJobs(foundIngr.skills);
 
   if (foundIngr.displayName) botResponse += `**${foundIngr.displayName}** ` ;
