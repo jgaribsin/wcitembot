19456e14a411a270e3c1caa7244777d2d1f33644
Major-Lue <Major-Lue@users.noreply.github.com>
2019-04-09 18:53:16 -0600

    ):<

--- commands/baseDamage.js
+++ commands/baseDamage.js
@@ -3,9 +3,13 @@ var fnc = require("./functions");
 module.exports.run = async (client, prefix, ingredients, ingredientNames, commands, items, message, args) => {
 
   if (args[0]) var level = parseInt(args[0]);
+  else message.channel.send("Please enter a level.");
   if (args[1]) var rarity = args[1].toString();
+  else message.channel.send("Please enter a rarity.");
   if (args[2]) var weaponType = args[2].toString();
+  else message.channel.send("Please enter a weapon type.");
   if (args[3]) var atkSpeed = args[3].toString();
+  else message.channel.send("Please enter an attack speed.");
 
   message.channel.send("Baseline damage is: " + fnc.calcBaseDam(level, rarity, weaponType, atkSpeed));
 }
--- commands/duraCalc.js
+++ commands/duraCalc.js
@@ -3,7 +3,9 @@ var fnc = require("./functions");
 module.exports.run = async (client, prefix, ingredients, ingredientNames, commands, items, message, args) => {
 
   if (args[0]) var level = parseInt(args[0]);
+  else message.channel.send("Please enter a level.");
   if (args[1]) var rarity = args[1].toString();
+  else message.channel.send("Please enter a tier.");
 
   if (level > 0 && (rarity > -1 && rarity < 4)) {
     message.channel.send("Durability cost for a level `" + level + "` `t" + rarity + "` ingredient is: " + fnc.durability(level, rarity) +
