220d68833e11f6aca6001d8dc99bf35f6493a251
Major-Lue <Major-Lue@users.noreply.github.com>
2019-04-09 19:54:02 -0600

    h spelling

--- commands/baseDamage.js
+++ commands/baseDamage.js
@@ -10,7 +10,7 @@ module.exports.run = async (client, prefix, ingredients, ingredientNames, comman
   if (args[2]) var weaponType = args[2].toString();
   else botResponse += "Please enter a weapon type.\n";
   if (args[3]) var atkSpeed = args[3].toString();
-  else botResponse += "Please enter a attack speed.\n";
+  else botResponse += "Please enter an attack speed.\n";
 
   if (args[0] && args[1] && args[2] && args[3])
   message.channel.send("Baseline damage is: " + fnc.calcBaseDam(level, rarity, weaponType, atkSpeed));
